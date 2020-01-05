d712444c05e1b2d558e284c5ebce83334c1d2cb2
John Frail Bongat <jfraildev@gmail.com>
2020-01-05 20:23:12 +0800

    Finished sass project

--- sass/modern_portfolio/dist/js/main.js
+++ sass/modern_portfolio/dist/js/main.js
@@ -5,7 +5,7 @@ const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
-// Set initial state of the menu
+// Set Initial State Of Menu
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
@@ -18,7 +18,7 @@ function toggleMenu() {
     menuBranding.classList.add('show');
     navItems.forEach(item => item.classList.add('show'));
 
-    // Set menu state
+    // Set Menu State
     showMenu = true;
   } else {
     menuBtn.classList.remove('close');
@@ -27,7 +27,7 @@ function toggleMenu() {
     menuBranding.classList.remove('show');
     navItems.forEach(item => item.classList.remove('show'));
 
-    // Set menu state
+    // Set Menu State
     showMenu = false;
   }
 }
