c756f9586e9c79c0159426350a500dedbcb2fe31
John Frail Bongat <jfraildev@gmail.com>
2020-01-10 22:54:27 +0800

    Started modern javascript course by traversy

---
+++ js/js_sandbox/js/app.js
@@ -0,0 +1,21 @@
+// Log to console
+console.log('Hello World!');
+console.log(123);
+console.log(true);
+
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1, 2, 3, 4]);
+
+console.log({ a: 1, b: 2 });
+
+<console className="blog"></console>
+
+<onsale className="table"></onsale>
+
+console.log(Thiss some error);
+
+console.logerror {}
+console.clear();
+
+console.log(error, clear, );
